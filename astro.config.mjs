06ec37276646eafdc0319462ee5586639366f926
YugenixZ <343198534@qq.com>
2026-05-02 01:13:38 +0200

    fix: set base for project repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://YugenixZ.github.io',
-  base: '/',
+  base: '/github.io',
 });
